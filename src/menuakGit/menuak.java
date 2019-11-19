97b95aecf787f1bd63f0a049dce6342ceb1dc418
lander <lander@DESKTOP-18MM8IU>
2019-11-19 13:40:40 +0100

    menú 4 opciones implementado

--- src/menuakGit/menuak.java
+++ src/menuakGit/menuak.java
@@ -1,9 +1,49 @@
 package menuakGit;
 
+import java.util.Scanner;
+
 public class menuak {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int aukera, zenb1, zenb2;
+		
+		double batura, kenketa, biderketa, zatiketa;
+		
+		System.out.println("Sartu zenbaki bat");
+		Scanner scan = new Scanner(System.in);
+		zenb1 = Integer.parseInt(scan.nextLine());
+		System.out.println("Sartu zenbaki bat");
+		Scanner scan1 = new Scanner(System.in);
+		zenb2 = Integer.parseInt(scan1.nextLine());
+		
+		batura = zenb1+zenb2;
+		kenketa = zenb1-zenb2;
+		biderketa = zenb1*zenb2;
+		zatiketa = zenb1/zenb2;
+		
+		
+		System.out.println("Aukeratu aukera bat \n0- Batura\n1- Kenketa\n2- Biderketa\n3- Zatiketa");
+		
+		aukera  = Integer.parseInt(scan.nextLine());
+		
+		switch(aukera){
+		case 0:
+			System.out.println("Batura: "  + batura);
+			break;
+		case 1:
+			System.out.println("Kenketa: " + kenketa);
+			break;
+		case 2:
+			System.out.println("Biderketa: " + biderketa);
+			break;
+		case 3:
+			System.out.println("Zatiketa: " + zatiketa);
+			break;
+		default:
+			System.out.println("Agur");
+			break;
+		}
+		
 
 	}
 
